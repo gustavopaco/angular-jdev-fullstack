ab02e14e73a58eec2114bde503fd6578cb49fd56
Gustavo Paco <gustavopaco@gmail.com>
2021-08-20 20:15:40 -0300

    - Implementando funcionalidade de Logout do sistema e redirecionamento para tela /login - Construcao da Nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,39 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-sb-microservicos';
+  private routes : Router
 
-  constructor() {
-
+  constructor(routes : Router) {
+    this.routes = routes;
   }
 
+  ngOnInit(): void {
+    const token = localStorage.getItem("token");
+    if (token !== null) {
+      this.routes.navigate(["home"]);
+    } else {
+      this.routes.navigate(["login"]);
+    }
+  }
 
+  public logout() {
+    localStorage.clear();
+    this.routes.navigate(["login"]);
+  }
+
+  public goHome() {
+    const token = localStorage.getItem("token");
+    if (token !== null) {
+      this.routes.navigate(["home"]);
+    } else {
+      this.routes.navigate(["login"]);
+    }
+  }
 }
